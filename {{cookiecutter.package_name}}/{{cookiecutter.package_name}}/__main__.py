5773f51f758e7d9a5aac86383c758fa6197b0f5a
Augusto Teixeira <augustoh.teixeira@gmail.com>
2016-09-26 14:46:57 -0300

    fix another pep issue

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__main__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+"""
+{{cookiecutter.package_name}}.__main__:
 
-"""{{cookiecutter.package_name}}.__main__:
-    executed when {{cookiecutter.package_name}} directory is called as script."""
+    executed when {{cookiecutter.package_name}} directory is called as script.
+"""
 
 
 from .{{cookiecutter.package_name}} import main
