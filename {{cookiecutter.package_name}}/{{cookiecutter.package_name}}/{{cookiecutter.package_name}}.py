62c0ee02b681b3f2df4c12ddecea54def6ac413b
Augusto Teixeira <augustoh.teixeira@gmail.com>
2016-09-26 14:39:08 -0300

    Fix some pep issues

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 
-
 """{{cookiecutter.package_name}}.{{cookiecutter.package_name}}: provides entry point main()."""
 
+import sys
 
 __version__ = "0.2.0"
 
 
-import sys
-from .stuff import Stuff
-
-
 def main():
+    """Docstring in public function."""
     print("Executing {{cookiecutter.package_name}} version %s." % __version__)
     print("List of argument strings: %s" % sys.argv[1:])
-    print("Stuff and Boo():\n%s\n%s" % (Stuff, Boo()))
-
-
-class Boo(Stuff):
-    pass
